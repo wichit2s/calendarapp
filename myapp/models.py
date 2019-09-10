b53474faa6ffa4dec49b26af4695cb20552edfb8
wichit2s <wichit.s@ubu.ac.th>
2019-09-10 22:09:06 +0700

    เสร็จภาค 2 แล้วนะทุกคน

--- myapp/models.py
+++ myapp/models.py
@@ -8,4 +8,4 @@ class Kijakum(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-      return f'{self.title} {self.date}'
+      return f'{self.title}'
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Kijakum
 
 # Create your views here.
 def index(req):
@@ -12,4 +13,5 @@ def dosomething(req):
     return HttpResponse('I do nothing')
 
 def myapp(req):
-    return render(req, 'myapp/myapp.html')
+    kijakums = Kijakum.objects.all()
+    return render(req, 'myapp/myapp.html', { 'kijakums': kijakums })
