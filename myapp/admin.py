a8b08fb77058c7c347b476e846c3438445aeb188
wichit2s <wichit.s@ubu.ac.th>
2019-09-10 21:16:30 +0700

    ตามให้ทัน

---
+++ myapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Kijakum
+
+# Register your models here.
+admin.site.register(Kijakum)
---
+++ myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Kijakum(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return f'{self.title} {self.date}'
